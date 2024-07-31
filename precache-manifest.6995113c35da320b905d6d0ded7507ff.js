5209577d99397b0dde0f245ffdce3afcfd2acabd
oengmengthong <oengmengthong@users.noreply.github.com>
2024-07-31 14:08:47 +0000

    deploy: d9f72faa46ec299a2682e982b2a896a2088ebbd3

---
+++ precache-manifest.6995113c35da320b905d6d0ded7507ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a10a79b89085c924ee8be2b19596ec",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "759a20a998835b8739e3",
+    "url": "/portfolio/static/css/main.34735358.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/portfolio/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/portfolio/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "759a20a998835b8739e3",
+    "url": "/portfolio/static/js/main.16afe081.chunk.js"
+  },
+  {
+    "revision": "e3943773e3b530223b78",
+    "url": "/portfolio/static/js/runtime~main.20d96849.js"
+  }
+]);
